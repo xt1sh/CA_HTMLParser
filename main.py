68250f73db9f2ea7edae8c80f21784bc106bcd06
xt1sh <azylav@gmail.com>
2020-06-16 17:28:33 +0300

    main upd

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from config import USE_GEVENT
 import time
 
 def main():
-	now = time.time()
-	manager = ParserManager('pages.xml', True)
+	manager = ParserManager('pages.xml', USE_GEVENT)
 	manager.parse()
-	print(time.time() - now)
-	now = time.time()
-	manager = ParserManager('pages.xml', False)
-	manager.parse()
-	print(time.time() - now)
-
 main()
