2f7477d76905d2831d81504be63039b7c29ce33f
Дима Винцык <dima.vintsyk@gmail.com>
2020-06-16 16:32:54 +0300

    use gevent

---
+++ config.py
@@ -0,0 +1,1 @@
+USE_GEVENT = False
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from parsermanager import ParserManager
 from parsers.metauaparser import MetaUaParser
 import datetime
 from optparse import OptionParser
-
+from config import USE_GEVENT
 
 def main():
-	manager = ParserManager('pages.xml')
-	manager.parse()
+	if not USE_GEVENT:
+		manager = ParserManager('pages.xml')
+		manager.parse()
 
 main()
